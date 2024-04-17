26844fcd849d7a75f5a832965a8d4248eff423dc
zackarychapple <zackarychapple30@gmail.com>
2024-04-16 22:13:09 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { withZephyr } = require('zephyr-webpack-plugin');
+
+module.exports = withZephyr()((env, options) => {
+  return {
+    entry: './src/main.tsx',
+    target: 'web',
+    mode: 'development',
+    watch: true,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [['postcss-preset-env']],
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    resolve: { extensions: ['.tsx', '.ts', '.js'] },
+    output: {
+      filename: 'main.bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: true,
+        minify: false,
+      }),
+    ],
+  };
+});
